304ffcdb53f59115514386918efa63e0e4b30616
Prashant Mishra <prashantmishra111@outlook.com>
2022-12-26 13:38:09 +0530

    Voice recognition in input added!!!

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
  
 import './App.css';
 import LandingPage from './component/landingPage';
+import Test from './component/speechRec';
 
 function App() {
   return (
     <div className="App">
        <LandingPage />
+       <Test />
     </div>
   );
 }
--- my-app/src/component/landingPage/Hero.js
+++ my-app/src/component/landingPage/Hero.js
@@ -1,14 +1,117 @@
-import React from 'react'
-
+import React from "react";
+import KeyboardVoiceIcon from "@mui/icons-material/KeyboardVoice";
+import CancelIcon from "@mui/icons-material/Cancel";
+import { useRef, useState } from "react";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+ 
 function Hero() {
+  const[input, setInput] = useState();
+  const [isListening, setIsListening] = useState(false);
+  const commands = [
+    {
+      command: "open *",
+      callback: (website) => {
+        window.open("http://" + website.split(" ").join(""));
+      },
+    },
+    {
+      command: "change background colour to *",
+      callback: (color) => {
+        document.body.style.background = color;
+      },
+    },
+    {
+      command: "reset",
+      callback: () => {
+        handleReset();
+      },
+    },
+
+    {
+      command: "reset background colour",
+      callback: () => {
+        document.body.style.background = `rgba(0, 0, 0, 0.8)`;
+      },
+    },
+  ];
+  const { transcript, resetTranscript } = useSpeechRecognition({ commands });
+   
+  const microphoneRef = useRef(null);
+  if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
+    return (
+      <div className="mircophone-container">
+        Browser is not Support Speech Recognition.
+      </div>
+    );
+  }
+  const handleListing = () => {
+    setIsListening(true);
+    microphoneRef.current.classList.add("listening");
+    SpeechRecognition.startListening({
+      continuous: true,
+    });
+  };
+  const stopHandle = () => {
+    setIsListening(false);
+    microphoneRef.current.classList.remove("listening");
+    SpeechRecognition.stopListening();
+  };
+  const handleReset = () => {
+    stopHandle();
+    resetTranscript("");
+  };
+
   return (
-    <div className= "hero-container">
-        <input 
-        placeholder='Type Location '
-        type="text" />
-        <button>Search</button>
+    <div className="hero-container">
+      <input
+        placeholder="Type Location "
+        type="text"
+        value={input || transcript}
+          onChange={(e) => setInput(e.target.value)}
+      />
+      
+
+      <KeyboardVoiceIcon
+        ref={microphoneRef}
+        onClick={handleListing}
+        // onDoubleClick={stopHandle}
+        sx={{
+          color: "red",
+          width: "4rem",
+          marginLeft: "-3.8rem",
+          marginTop: "2rem",
+          height: "2rem",
+          fontSize: "2.6rem",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      />
+       {(input && input.length > 0) || (transcript && transcript.length > 0) ? (
+        <CancelIcon
+          onClick={() =>{
+            stopHandle();
+            resetTranscript("");
+            setInput("");
+          }}
+          sx={{
+            color: "red",
+            // marginLeft: "-2rem",
+            padding: "0.3rem",
+            height: "2rem",
+
+            fontSize: "1 rem",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        />
+      ) : (
+        ""
+      )}
+      <button>Search</button>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
